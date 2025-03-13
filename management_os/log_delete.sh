12e10b64f82b34dee984b3d773521c4c0b84eee8
helperchoi <144641808+infra-se@users.noreply.github.com>
2025-03-13 15:41:29 +0900

    Update log_delete.sh

--- management_os/log_delete.sh
+++ management_os/log_delete.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 #Script by helperchoi@gmail.com
 #Script Description : log delete script
-SCRIPT_VER=0.1.20240813
+SCRIPT_VER=0.3.20250313
 
 export LANG=C
 export LC_ALL=C
 
-LOG_DIR_LIST="/logs/user_logs /logs/iptables"
+LOG_DIR=/root/shell/logs
+LOG_DATE=`date +%Y%m%d_%H%M%S`
+LOG_NAME=${LOG_DATE}_log_delete.log
+mkdir -p ${LOG_DIR}
+
+LOG_DIR_LIST="/logs/syslog /logs/iptables"
 LOG_LIMIT_DAY=30
 
 FUNCT_LOG_DELETE() {
 	LOG_DIR=$1
-	find ${LOG_DIR} -type f -mtime +${LOG_LIMIT_DAY} -exec rm -f {} \;
+	DELETE_LIST=/tmp/delete_backup.list
+
+	find ${LOG_DIR} -type f -mtime +${LOG_LIMIT_DAY} > ${DELETE_LIST}
+	
+	CHECK_DELETE_LIST=`cat ${DELETE_LIST} | wc -l`
+	
+	if [ ${CHECK_DELETE_LIST} -gt 0 ]
+	then
+		echo  "[INFO] ${LOG_DATE} Delete Log List : ${LOG_DIR}"
+		cat ${DELETE_LIST}
+
+		for D_LIST in `cat ${DELETE_LIST}`
+		do
+			echo "Delete : ${D_LIST}"
+			rm -f ${D_LIST}
+		done
+	else
+		echo  "[INFO] ${LOG_DATE} There is no file to delete : ${LOG_DIR}"
+	fi
 }
 
-for LIST in ${LOG_DIR_LIST}
-do
-	FUNCT_LOG_DELETE ${LIST}
-done
+FUNCT_MAIN() {
+	echo "### Run Delete Log Script ###"
+
+	for LIST in ${LOG_DIR_LIST}
+	do
+		FUNCT_LOG_DELETE ${LIST}
+	done
+}
+
+FUNCT_MAIN | tee -a ${LOG_DIR}/${LOG_NAME}
