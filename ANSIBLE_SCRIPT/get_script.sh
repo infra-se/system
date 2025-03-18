3846b26d1c9027a6faadefc5f1b6ed54b34dbc05
helperchoi <144641808+infra-se@users.noreply.github.com>
2025-03-18 13:07:41 +0900

    Update get_script.sh

--- ANSIBLE_SCRIPT/get_script.sh
+++ ANSIBLE_SCRIPT/get_script.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 #Script made by helperchoi@gmail.com
 SCRIPT_DESCRIPTION="Ansible Script Initialize"
-SCRIPT_VER=0.5.20240125
+SCRIPT_VER=0.6.20250318
 WORK_PATH=/home/${USER}/ANSIBLE_SCRIPT
 
 export LANG=C
 export LC_ALL=C
+
+FUNCT_CHECK_OS() {
+	CHECK_OS=`uname -a | cut -d "/" -f 2 | tr '[A-Z]' '[a-z]'`
+
+	if [ $CHECK_OS = "linux" ];
+	then
+		if [ `grep -i "ubuntu" /etc/*-release| wc -l` -gt 0 ]
+		then
+			export OS_PLATFORM="UBUNTU"
+		else
+			export OS_PLATFORM="ROCKY"
+		fi
+	else
+		echo "[Error] Can not execute. run script is only Linux OS"
+		exit 1
+	fi
+}
+
+FUNCT_CHECK_CMD() {
+        TARGET_LIST=git
+	${TARGET_LIST} > /dev/null 2>&-
+
+        if [ $? -eq 1 ]
+        then
+                export CHECK_RESULT=0
+        else
+                export CHECK_RESULT=1
+        fi
+}
 
 if [ "${USER}" == "root" ]
 then
@@ -14,6 +43,23 @@ then
 	echo
 	exit 1
 else
+	FUNCT_CHECK_OS
+	FUNCT_CHECK_CMD
+
+	if [ ${CHECK_RESULT} -eq 1 ]
+ 	then
+		if [ ${OS_PLATFORM} == "ROCKY" ]
+  		then
+			yum -y install git
+    		elif [ ${OS_PLATFORM} == "UBUNTU" ]
+      		then
+			apt-get -y install git
+   		else
+			echo "[ERR] Not Support OS"
+   			exit 1
+      		fi
+  	fi
+
 	echo
 	echo "[INFO] Download the Ansible script to the top path under the user account."
 	echo "(${WORK_PATH})"
