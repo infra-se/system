141acf07416ac0d7563b6ce249575ea921ecb44c
helperchoi <144641808+infra-se@users.noreply.github.com>
2025-03-18 13:06:10 +0900

    Update get_script.sh

--- MANUAL_CI_COLLECT/get_script.sh
+++ MANUAL_CI_COLLECT/get_script.sh
@@ -1,13 +1,59 @@
 #!/bin/bash
 #Script made by helperchoi@gmail.com
 SCRIPT_DESCRIPTION="Get Manual CI Collect Script"
-SCRIPT_VER=0.3.20240125
+SCRIPT_VER=0.4.20250318
 
 export LANG=C
 export LC_ALL=C
 
+FUNCT_CHECK_OS() {
+	CHECK_OS=`uname -a | cut -d "/" -f 2 | tr '[A-Z]' '[a-z]'`
+
+	if [ $CHECK_OS = "linux" ];
+	then
+		if [ `grep -i "ubuntu" /etc/*-release| wc -l` -gt 0 ]
+		then
+			export OS_PLATFORM="UBUNTU"
+		else
+			export OS_PLATFORM="ROCKY"
+		fi
+	else
+		echo "[Error] Can not execute. run script is only Linux OS"
+		exit 1
+	fi
+}
+
+FUNCT_CHECK_CMD() {
+        TARGET_LIST=git
+	${TARGET_LIST} > /dev/null 2>&-
+
+        if [ $? -eq 1 ]
+        then
+                export CHECK_RESULT=0
+        else
+                export CHECK_RESULT=1
+        fi
+}
+
 if [ "${USER}" == "root" ]
 then
+	FUNCT_CHECK_OS
+	FUNCT_CHECK_CMD
+
+	if [ ${CHECK_RESULT} -eq 1 ]
+ 	then
+		if [ ${OS_PLATFORM} == "ROCKY" ]
+  		then
+			yum -y install git
+    		elif [ ${OS_PLATFORM} == "UBUNTU" ]
+      		then
+			apt-get -y install git
+   		else
+			echo "[ERR] Not Support OS"
+   			exit 1
+      		fi
+  	fi
+
 	WORK_PATH=/root/shell
 	mkdir -p ${WORK_PATH}
 	cd ${WORK_PATH}
