35816a2c4efa92cf3f3cb4bb67bd83bc4a412e4b
helperchoi <144641808+infra-se@users.noreply.github.com>
2025-04-25 11:59:24 +0900

    Update check_cve_patch.sh

--- management_os/check_cve_patch.sh
+++ management_os/check_cve_patch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Script made by helperchoi@gmail.com
 SCRIPT_DESCRIPTION="Search CVE Patch Code"
-SCRIPT_VER=0.2.20250422
+SCRIPT_VER=0.3.20250425
 
 export LANG=C
 export LC_ALL=C
 
-VAR_CVE_CODE="CVE-2024-53150 CVE-2024-53197"
+VAR_CVE_CODE=$1
 PATTERN=$(echo "${VAR_CVE_CODE}" | sed 's/ /|/g')
 CVE_COUNT=$(echo "${VAR_CVE_CODE}" | wc -w)
 
@@ -28,6 +28,23 @@ FUNCT_CHECK_OS() {
 		echo "[ERROR] ${HOSTNAME} Can not execute. run script is only Linux OS"
 		exit 1
 	fi
+}
+
+FUNCT_CHECK_CVE_PATTERN() {
+	VAR_CVE_CODE=$1
+
+	CHECK_PATTERN_RESULT=0
+
+	for LIST in ${VAR_CVE_CODE}
+	do
+		CHECK_PATTERN=`echo "${LIST}" | grep -o "^CVE-[0-9]\{4\}-[0-9]\{4,5\}$" | wc -l`
+
+		if [ ${CHECK_PATTERN} -ne 1 ]
+		then
+			echo "[ERROR] ${HOSTNAME} The input type did not meet the CVE code pattern. : ${LIST}"
+			CHECK_PATTERN_RESULT=1
+		fi
+	done
 }
 
 FUNCT_SHOW_PROGRESS() {
@@ -65,7 +82,39 @@ FUNCT_SEARCH_CVE() {
 
 }
 
-FUNCT_CHECK_OS
+FUNCT_MANDATORY() {
+	if [ "${USER}" != "root" ]
+	then
+		echo
+		echo "[ERROR] ${HOSTNAME} This script must be used in a Only 'root' Account."
+		echo
+		exit 1
+	else
+		if [ -z "${VAR_CVE_CODE}" ]
+		then
+			echo
+			echo "[ERROR] ${HOSTNAME} CVE Code was not Input."
+			echo
+			echo "Usage ) : $0 \"CVE-2024-53150 CVE-2024-53197\""
+			echo
+			exit 1
+		else
+			FUNCT_CHECK_CVE_PATTERN "${VAR_CVE_CODE}"
+
+			if [ ${CHECK_PATTERN_RESULT} -ne 0 ]
+			then
+				echo "[INFO] ${HOSTNAME} The input type can only be the CVE Code Pattern. : ex) CVE-2024-53150 CVE-2024-53197"
+				echo
+				exit 1
+			else
+				FUNCT_CHECK_OS
+			fi
+		fi
+	fi
+
+}
+
+FUNCT_MANDATORY
 FUNCT_SHOW_PROGRESS &
 PROGRESS_PID=$!
 
