88a1fd9210690735cb6c852cab592244a46da45c
helperchoi <144641808+infra-se@users.noreply.github.com>
2025-07-03 13:45:43 +0900

    Update sec_std_conf.sh

--- KISA_SECURITY/sec_std_conf.sh
+++ KISA_SECURITY/sec_std_conf.sh
@@ -34,7 +34,8 @@ FUNCT_CHECK_OS() {
 			ubuntu) OS_PLATFORM="UBUNTU" ;;
 			rocky) OS_PLATFORM="RHEL" ;;
 			centos) OS_PLATFORM="RHEL" ;;
-   			rhel) OS_PLATFORM="RHEL" ;;
+			rhel) OS_PLATFORM="RHEL" ;;
+			amzn) OS_PLATFORM="RHEL" ;;
 			*) echo "[ERROR] ${HOSTNAME} Unsupported Linux"; exit 1 ;;
 		esac
 
